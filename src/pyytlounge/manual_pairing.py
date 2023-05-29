57c2270671ca3b7466f911839ba013fe68ea1d4b
dmunozv04 <39565245+dmunozv04@users.noreply.github.com>
2023-05-29 17:10:41 +0200

    remove the need for requests

--- src/pyytlounge/manual_pairing.py
+++ src/pyytlounge/manual_pairing.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
-import requests
+import asyncio
+import aiohttp
 from api import api_base
-
+async def get_screen(pairing_code):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f"{api_base}/pairing/get_screen", data={'pairing_code': pairing_code}) as response:
+            return await response.json()
 pairing_code = input("Enter manual pairing code as shown in YouTube app on target: ")
 try:
     pairing_code = int(pairing_code)
@@ -10,8 +14,9 @@ except Exception as ex:
     print(f"Invalid pairing code {pairing_code}", ex)
 
 # initial paring with code
-res = requests.post(f"{api_base}/pairing/get_screen", data={'pairing_code': pairing_code})
-screens = res.json()
+loop = asyncio.get_event_loop()
+
+screens = loop.run_until_complete(get_screen(pairing_code))
 screen = screens["screen"]
 screen_name = screen["name"]
 screen_id = screen["screenId"]
