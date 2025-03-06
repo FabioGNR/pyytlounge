112cd0523c544a71ed300646b1adeaae71a71ac6
Fabio <fabio.waljaard@gmail.com>
2025-03-06 22:01:51 +0000

    Add onVolumeChanged and onAutoplayModeChanged events

--- src/pyytlounge/__init__.py
+++ src/pyytlounge/__init__.py
@@ -1,4 +1,9 @@
 from .wrapper import YtLoungeApi, get_thumbnail_url
 from .event_listener import EventListener
-from .events import PlaybackStateEvent, NowPlayingEvent
+from .events import (
+    PlaybackStateEvent,
+    NowPlayingEvent,
+    VolumeChangedEvent,
+    AutoplayModeChangedEvent,
+)
 from .models import State
--- src/pyytlounge/events.py
+++ src/pyytlounge/events.py
@@ -2,7 +2,12 @@ import logging
 
 from .api import get_thumbnail_url
 from .models import State
-from .lounge_models import _NowPlayingData, _PlaybackStateData
+from .lounge_models import (
+    _NowPlayingData,
+    _PlaybackStateData,
+    _VolumeChangedData,
+    _AutoplayModeChangedData,
+)
 
 _logger = logging.getLogger(__name__)
 
@@ -31,3 +36,19 @@ class NowPlayingEvent(PlaybackStateEvent):
         """Returns thumbnail for current video. Use thumbnail idx to get different thumbnails."""
 
         get_thumbnail_url(self.video_id, thumbnail_idx=thumbnail_idx)
+
+
+class VolumeChangedEvent:
+    """Contains information related to volume"""
+
+    def __init__(self, data: _VolumeChangedData):
+        self.volume: int = data["volume"]
+        self.muted: bool = data["muted"] == "true"
+
+
+class AutoplayModeChangedEvent:
+    """Contains auto play mode"""
+
+    def __init__(self, data: _AutoplayModeChangedData):
+        self.enabled: bool = data["autoplayMode"] == "ENABLED"
+        self.supported: bool = data["autoplayMode"] != "UNSUPPORTED"
