c74177229ae102dd0e9c392c679677bdf0d713e7
Fabio <fabio.waljaard@gmail.com>
2025-03-06 22:09:37 +0000

    Fix pytests

--- tests/test_process_event.py
+++ tests/test_process_event.py
@@ -5,8 +5,8 @@ from .devices import Device, devices
 
 
 @pytest.mark.parametrize("device", devices.values(), ids=devices.keys())
-def test_event_loungeStatus(api: YtLoungeApi, device: Device):
-    api._process_event("loungeStatus", [device.data["loungeStatus"]])
+async def test_event_loungeStatus(api: YtLoungeApi, device: Device):
+    await api._process_event("loungeStatus", [device.data["loungeStatus"]])
 
     assert api._screen_name == device.expectations["screen_name"]
     assert api._device_info == device.expectations["device_info"]
