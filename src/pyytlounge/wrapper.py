86274c916048cadff29d23abfff6325c28fc2f9a
Matthew Stern <matt.st97@gmail.com>
2025-03-17 22:06:28 -0400

    feat: enumerate available caption tracks

--- src/pyytlounge/wrapper.py
+++ src/pyytlounge/wrapper.py
@@ -6,7 +6,7 @@ import logging
 from typing import Any, AsyncIterator, Dict, List, Optional
 
 import aiohttp
-from aiohttp import ClientTimeout, ClientPayloadError
+from aiohttp import ClientTimeout, ClientPayloadError, ClientSession
 
 from .api import api_base
 from .api import get_thumbnail_url  # noqa # we want to export this from this module
@@ -465,3 +465,28 @@ class YtLoungeApi:
         video_id is always required.
         """
         return await self._command("setSubtitlesTrack", {"languageCode": language_code, "videoId": video_id})
+    
+    async def get_available_captions(self, api_key: str, video_id: str):
+        """Uses the traditional YouTube API to enumerate available subtitle tracks."""
+        yt_base_url = "https://www.googleapis.com/youtube/v3/captions"
+        params = {
+            "part": "snippet",
+            "videoId": video_id,
+            "key": api_key
+        }
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(yt_base_url, params=params) as response:
+                if response.status != 200:
+                    raise Exception(f"Request failed with status {response.status}")
+                
+                data = await response.json()
+                
+                languages = []
+                for item in data.get("items", []):
+                    snippet = item.get("snippet", {})
+                    language = snippet.get("language")
+                    if language and language not in languages:
+                        languages.append(language)
+                        
+                return languages
